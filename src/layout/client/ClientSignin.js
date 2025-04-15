4c085762a26e3e9b11b2510fa0cadf7c1e79d58a
Senior Mobile app | Web | API Developer <seniordev52@gmail.com>
2025-04-15 12:32:37 +0300

    fix

--- src/layout/client/ClientSignin.js
+++ src/layout/client/ClientSignin.js
@@ -218,7 +218,7 @@ export default function ClientSignIn({ navigation }) {
   return (
     <View style={styles.container}>
       <StatusBar  translucent backgroundColor="transparent" />
-      <MHeader navigation={navigation}/>
+      <MHeader navigation={navigation} back={true}/>
       <ScrollView style = {styles.scroll}
         showsVerticalScrollIndicator={false}
       >
