c253f5e664577ca94f66d0e40b24d518ea487b87
Senior Mobile app | Web | API Developer <techableteam@gmail.com>
2025-08-04 14:50:32 +0300

    fix url to team schdule in hotel

--- src/layout/hospitality/hotel/hire/hospitality_hotel_hire_home.js
+++ src/layout/hospitality/hotel/hire/hospitality_hotel_hire_home.js
@@ -59,7 +59,7 @@ export default function HospitalityHotelHireHome ({ navigation }) {
               <ImageButton title={"APPROVE SHIFTS"} onPress={() => handleNavigate('HospitalityHotelHireEditShift')} />
             </View>
             <View style={styles.buttonWrapper}>
-              <ImageButton title={"TEAM SCHEDULER"} onPress={() => handleNavigate('HospitalityRestaurantHireTimeSchduler')} />
+              <ImageButton title={"TEAM SCHEDULER"} onPress={() => handleNavigate('HospitalityRestaurantHireSchedulerScreen')} />
             </View>
           </View>
           <View style={styles.profile}>
